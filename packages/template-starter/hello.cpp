a1884966226e43441654d3b07d2fdf7b6d1b4d68
Kim Brandwijk <kim.brandwijk@gmail.com>
2026-04-02 19:24:38 -0500

    template rename

--- packages/template-starter/hello.cpp
+++ packages/template-starter/hello.cpp
@@ -4,12 +4,12 @@
 #include "Nativ.h"
 #include <string>
 
-RNA_EXPORT(sync)
+NATIV_EXPORT(sync)
 int add(int a, int b) {
     return a + b;
 }
 
-RNA_EXPORT(sync)
+NATIV_EXPORT(sync)
 std::string greet(const std::string& name) {
     return "Hello " + name + " from C++!";
 }
